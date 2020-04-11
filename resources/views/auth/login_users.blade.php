23554866910336db7964e50218ae18add3b00d34
ricar <ricardinhorl@hotmail.com>
2020-04-11 18:14:57 -0300

    Adding pictures

---
+++ resources/views/auth/login_users.blade.php
@@ -0,0 +1,73 @@
+@extends('layouts.index')
+
+@section('layout')
+    <div class="container padding1">
+        <h1 class="display-4 text-center"><span class="text-light">Ever</span><span class="text-success">Green</span></h1>
+        <div class="row justify-content-center">
+            <div class="col-md-4">
+                <div class="card">
+                    <div class="card-body">
+
+                        @if($current == "student")
+                            <h3 class="card-title text-center">Portal do Aluno</h3><br>
+                            <form method="POST" action="{{ route('student.login.submit') }}">
+                        @elseif($current == "teacher")
+                            <h3 class="card-title text-center">Portal do Professor</h3><br>
+                            <form method="POST" action="{{ route('teacher.login.submit') }}">
+                        @endif
+
+                        @csrf
+
+                        <div class="form-group row">
+                            <label for="email" class="col-md-4 col-form-label text-md-right">E-mail</label>
+                            <div class="col-md-6">
+                                <input id="email" type="email" class="form-control border-success @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+                                @error('email')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <label for="password" class="col-md-4 col-form-label text-md-right">Senha</label>
+                            <div class="col-md-6">
+                                <input id="password" type="password" class="form-control border-success @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
+                                @error('password')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <div class="col-md-6 offset-md-4">
+                                <div class="form-check">
+                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
+                                    <label class="form-check-label" for="remember">Lembrar-me</label>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="form-group row mb-0">
+                            <div class="col-md-8 offset-md-4">
+                                <button type="submit" class="btn btn-success">Entrar</button>
+                            </div>
+                        </div>
+
+                        <div class="form-group row mb-0">
+                            <div class="col-md-8 offset-md-2">
+                                @if (Route::has('password.request'))
+                                    <a class="btn btn-link" href="{{ route('password.request') }}">Esqueceu-se da senha?</a>
+                                @endif
+                            </div>
+                        </div>
+                    </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection

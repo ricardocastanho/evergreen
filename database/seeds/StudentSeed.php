756a6f742cb339ba1f36209413081c3e371bc961
ricar <ricardinhorl@hotmail.com>
2020-04-21 18:24:45 -0300

    Adding images profiles

--- database/seeds/StudentSeed.php
+++ database/seeds/StudentSeed.php
@@ -13,8 +13,6 @@ class StudentSeed extends Seeder
     {
         DB::table('students')->insert(['name'=>'Ricardo Castanho Soares', 'email'=>'ricardo@gmail.com', 'password'=>Hash::make('12345678'), 'course_id'=>1, 'image'=>'images/ricardo.jpeg']);
         DB::table('students')->insert(['name'=>'Rafaella', 'email'=>'rafa@gmail.com', 'password'=>Hash::make('12345678'), 'course_id'=>1, 'image'=>'images/without-img.jpg']);
-        DB::table('students')->insert(['name'=>'Raziel', 'email'=>'razi@gmail.com', 'password'=>Hash::make('12345678'), 'course_id'=>1, 'image'=>'images/without-img.jpg']);
-        DB::table('students')->insert(['name'=>'Leonardo', 'email'=>'leo@gmail.com', 'password'=>Hash::make('12345678'), 'course_id'=>2, 'image'=>'images/without-img.jpg']);
-        DB::table('students')->insert(['name'=>'Paulo', 'email'=>'paulo@gmail.com', 'password'=>Hash::make('12345678'), 'course_id'=>3, 'image'=>'images/without-img.jpg']);
+        DB::table('students')->insert(['name'=>'Raziel', 'email'=>'razi@gmail.com', 'password'=>Hash::make('12345678'), 'course_id'=>1, 'image'=>'images/raziel.png']);
     }
 }
